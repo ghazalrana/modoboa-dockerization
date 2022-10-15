afc9017d20b55eeb9ade8f4f60303ef7f70d9c74
c0dehammer <50229753+c0dehammer@users.noreply.github.com>
2022-10-15 17:17:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sed -i "0,/mail.localhost/ s/mail.localhost/${DOMAIN}/" /etc/postfix/main.cf
-sed -i "0,/mail.localhost/ s/mail.localhost/${DOMAIN}/" /etc/amavis/conf.d/05-node_id
-sed -i "s/server_name mail.localhost/server_name ${DOMAIN}/g" /etc/nginx/sites-enabled/mail.localhost.conf
-domain=${DOMAIN}
-domain=$(expr match "$domain" '.*\.\(.*\..*\)')
-sed -i "s/server_name autoconfig.localhost/server_name autoconfig.${domain}/g" /etc/nginx/sites-enabled/autoconfig.localhost.conf
+#sed -i "0,/mail.localhost/ s/mail.localhost/${DOMAIN}/" /etc/postfix/main.cf
+#sed -i "0,/mail.localhost/ s/mail.localhost/${DOMAIN}/" /etc/amavis/conf.d/05-node_id
+#sed -i "s/server_name mail.localhost/server_name ${DOMAIN}/g" /etc/nginx/sites-enabled/mail.localhost.conf
+#domain=${DOMAIN}
+#domain=$(expr match "$domain" '.*\.\(.*\..*\)')
+#sed -i "s/server_name autoconfig.localhost/server_name autoconfig.${domain}/g" /etc/nginx/sites-enabled/autoconfig.localhost.conf
 
 services=("cron" "nginx" "postgresql ""uwsgi" "dovecot" "postfix" "amavis" "opendkim" "clamav-daemon")
 
